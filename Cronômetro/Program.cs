2bae3c2d0d659da228c90742efb7d38b6c3567a1
Iraponan Aires de Souza Marinho <iraponan.aires@outlook.com>
2022-04-15 23:10:19 -0300

    Criando a função de iniciar o cronômetro.

--- Cronômetro/Program.cs
+++ Cronômetro/Program.cs
@@ -2,11 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Cronômetro {
     internal class Program {
         static void Main(string[] args) {
+            iniciar();
+            //Console.ReadLine();
+        }
+
+        static void iniciar() {
+            int tempo = 10;
+            int tempoAtual = 0;
+
+            while (tempoAtual != tempo) { 
+                Console.Clear();
+                tempoAtual++;
+                Console.WriteLine(tempoAtual);
+                Thread.Sleep(1000);
+            }
         }
     }
 }
